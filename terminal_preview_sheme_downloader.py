80d6cfc626e33601cc535ff9a1f7083eef0767a3
Andreas Guth <9ordin@gmail.com>
2019-12-16 22:01:25 +0100

    Uncommented version that downloads the files...

--- terminal_preview_sheme_downloader.py
+++ terminal_preview_sheme_downloader.py
@@ -123,5 +123,5 @@ class WindowsTerminalSchemeDownloader(object):
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     downloader = WindowsTerminalSchemeDownloader()
-    downloader.download_and_add_schemes_to_config(repo_path='C:\\Users\\Gordin\\AppData\\Local\\Temp\\tmpyteiipoy\\', keep_repo=True)
-    # downloader.download_and_add_schemes_to_config(keep_repo=True)
+    # downloader.download_and_add_schemes_to_config(repo_path='C:\\Users\\Gordin\\AppData\\Local\\Temp\\tmpyteiipoy\\', keep_repo=True)
+    downloader.download_and_add_schemes_to_config(keep_repo=True)
