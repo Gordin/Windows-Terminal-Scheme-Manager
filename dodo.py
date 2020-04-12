440cb2fc412b39cf9a2e8e83a74fe7507d1e0870
Gordin <9ordin@gmail.com>
2020-04-13 00:14:32 +0200

    dodo file working for generating .exe file

--- dodo.py
+++ dodo.py
@@ -10,13 +10,27 @@ def _source_files():
 def task_build_onefile():
     return {
         'file_dep': _source_files(),
-        'targets': [path.join('.', 'dist', 'scheme_manager.exe')],
+        'targets': [path.join('.', 'dist', 'wtsm.exe')],
         'actions': [
-            'pyinstaller --onefile --hidden-import="pkg_resources.py2_warn" {}'
-            .format(path.join(
+            'pyinstaller --onefile \
+            --noupx\
+            --hidden-import="pkg_resources.py2_warn" \
+            --name wtsm \
+            {}\
+            '.format(path.join(
                 '.', 'windows_terminal_scheme_manager', 'scheme_manager.py'
             ))]
     }
+
+
+def task_build_onefile_upx():
+    task = task_build_onefile()
+    task['actions'][0] = task['actions'][0].replace('--noupx', '\
+        --upx-dir=C:\\Users\\Gordin\\Apps\\upx-3.96-win64 \
+        --upx-exclude=vcruntime140.dll \
+        --upx-exclude=python38.dll \
+    ')
+    return task
 
 
 def task_test():
